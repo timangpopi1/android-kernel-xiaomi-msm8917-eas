785faaf2c0309fa984bbe1d4aa552d439d3b237f
kdrag0n <dragon@khronodragon.com>
2018-05-18 22:00:23 -0700

    printk: add live printk toggle
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -49,6 +49,8 @@
 
 #include <asm/uaccess.h>
 
+#include "../printk_interface.h"
+
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1182,7 +1184,7 @@ static int syslog_print(char __user *buf, int size)
 	return len;
 }
 
-static int syslog_print_all(char __user *buf, int size, bool clear)
+int syslog_print_all(char __user *buf, int size, bool clear)
 {
 	char *text;
 	int len = 0;
@@ -1634,6 +1636,10 @@ asmlinkage int vprintk_emit(int facility, int level,
 			    const char *dict, size_t dictlen,
 			    const char *fmt, va_list args)
 {
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
+
 	static int recursion_bug;
 	static char textbuf[LOG_LINE_MAX];
 	char *text = textbuf;
@@ -1809,6 +1815,10 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
+
 	return vprintk_emit(0, -1, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
@@ -1817,6 +1827,10 @@ asmlinkage int printk_emit(int facility, int level,
 			   const char *dict, size_t dictlen,
 			   const char *fmt, ...)
 {
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
+
 	va_list args;
 	int r;
 
@@ -1854,6 +1868,10 @@ asmlinkage __visible int printk(const char *fmt, ...)
 	va_list args;
 	int r;
 
+	// if printk mode is disabled, terminate instantly
+	if (printk_mode == 0)
+		return 0;
+
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
 		va_start(args, fmt);
@@ -1862,6 +1880,7 @@ asmlinkage __visible int printk(const char *fmt, ...)
 		return r;
 	}
 #endif
+
 	va_start(args, fmt);
 	r = vprintk_emit(0, -1, NULL, 0, fmt, args);
 	va_end(args);
