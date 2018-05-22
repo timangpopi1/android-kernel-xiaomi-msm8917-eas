02f196314baf663825752c39a78d6a7db63e0e3d
Patrick Bellasi <patrick.bellasi@arm.com>
2018-05-22 12:07:53 +0100

    UPSTREAM: cpufreq: schedutil: Fix iowait boost reset
    A more energy efficient update of the IO wait boosting mechanism has
    been introduced in:
    
       commit a5a0809bc58e ("cpufreq: schedutil: Make iowait boost more energy efficient")
    
    where the boost value is expected to be:
    
     - doubled at each successive wakeup from IO
       staring from the minimum frequency supported by a CPU
    
     - reset when a CPU is not updated for more then one tick
       by either disabling the IO wait boost or resetting its value to the
       minimum frequency if this new update requires an IO boost.
    
    This approach is supposed to "ignore" boosting for sporadic wakeups from
    IO, while still getting the frequency boosted to the maximum to benefit
    long sequence of wakeup from IO operations.
    
    However, these assumptions are not always satisfied.
    For example, when an IO boosted CPU enters idle for more the one tick
    and then wakes up after an IO wait, since in sugov_set_iowait_boost() we
    first check the IOWAIT flag, we keep doubling the iowait boost instead
    of restarting from the minimum frequency value.
    
    This misbehavior could happen mainly on non-shared frequency domains,
    thus defeating the energy efficiency optimization, but it can also
    happen on shared frequency domain systems.
    
    Let fix this issue in sugov_set_iowait_boost() by:
     - first check the IO wait boost reset conditions
       to eventually reset the boost value
     - then applying the correct IO boost value
       if required by the caller
    
    Fixes: a5a0809bc58e (cpufreq: schedutil: Make iowait boost more energy efficient)
    Reported-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: Mhmmdfadlyas <mhmmdfas@pm.me>
    
    Conflicts:
    	kernel/sched/cpufreq_schedutil.c

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -257,7 +257,8 @@ static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time)
 		}
 	}
 
-	if (sg_cpu->flags & SCHED_CPUFREQ_IOWAIT) {
+	if (flags & SCHED_CPUFREQ_IOWAIT) {
+
 		if (sg_cpu->iowait_boost_pending)
 			return;
 
