4bc12eeb280b5290f6bb76a409691753f4e3d6e1
Mhmmdfadlyas <mhmmdfadlyas@pm.me>
2019-12-13 13:55:48 +0700

    BACKPORT: add some scripts for governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -554,6 +554,9 @@ extern struct cpufreq_governor cpufreq_gov_helix_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKUTIL)
 extern struct cpufreq_governor cpufreq_gov_darkutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_darkutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PWRUTILX)
+extern struct cpufreq_governor cpufreq_gov_pwrutilx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pwrutilx)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
