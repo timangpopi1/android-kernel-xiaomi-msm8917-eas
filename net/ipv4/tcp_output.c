534a2daf467d5c119eb2c8838845dfdec52c0c08
Mhmmdfadlyas <mhmmdfas@pm.me>
2019-12-24 03:34:02 +0700

    Revert "fix merge"
    This reverts commit afe42fa4923bae627dcb8507c160f7b72625a85e.

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1145,6 +1145,7 @@ int tcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 	struct tcp_sock *tp = tcp_sk(sk);
 	struct sk_buff *buff;
 	int nsize, old_factor;
+	long limit;
 	int nlen;
 	u8 flags;
 
@@ -1155,7 +1156,8 @@ int tcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 	if (nsize < 0)
 		nsize = 0;
 
-		if (unlikely((sk->sk_wmem_queued >> 1) > sk->sk_sndbuf + 0x20000)) {
+		
+		if (unlikely((sk->sk_wmem_queued >> 1) > sk->sk_sndbuf)) {
 		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPWQUEUETOOBIG);
 		return -ENOMEM;
 	}
