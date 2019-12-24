8e149a4a332691dfb474103ff435a98db7168d76
Mhmmdfadlyas <mhmmdfas@pm.me>
2019-12-24 00:25:17 +0000

    Revert "printk: add live printk toggle"
    This reverts commit 785faaf2c0309fa984bbe1d4aa552d439d3b237f.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -49,8 +49,6 @@
 
 #include <asm/uaccess.h>
 
-#include "../printk_interface.h"
-
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -1188,7 +1186,7 @@ static int syslog_print(char __user *buf, int size)
 	return len;
 }
 
-int syslog_print_all(char __user *buf, int size, bool clear)
+static int syslog_print_all(char __user *buf, int size, bool clear)
 {
 	char *text;
 	int len = 0;
@@ -1640,10 +1638,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 			    const char *dict, size_t dictlen,
 			    const char *fmt, va_list args)
 {
-	/* return instantly if printk is disabled */
-	if (likely(printk_mode == 0))
-		return 0;
-
 	static int recursion_bug;
 	static char textbuf[LOG_LINE_MAX];
 	char *text = textbuf;
@@ -1819,10 +1813,6 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
-	/* return instantly if printk is disabled */
-	if (likely(printk_mode == 0))
-		return 0;
-
 	return vprintk_emit(0, -1, NULL, 0, fmt, args);
 }
 EXPORT_SYMBOL(vprintk);
@@ -1831,10 +1821,6 @@ asmlinkage int printk_emit(int facility, int level,
 			   const char *dict, size_t dictlen,
 			   const char *fmt, ...)
 {
-	/* return instantly if printk is disabled */
-	if (likely(printk_mode == 0))
-		return 0;
-
 	va_list args;
 	int r;
 
@@ -1872,10 +1858,6 @@ asmlinkage __visible int printk(const char *fmt, ...)
 	va_list args;
 	int r;
 
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-		return 0;
-
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
 		va_start(args, fmt);
@@ -1884,7 +1866,6 @@ asmlinkage __visible int printk(const char *fmt, ...)
 		return r;
 	}
 #endif
-
 	va_start(args, fmt);
 	r = vprintk_emit(0, -1, NULL, 0, fmt, args);
 	va_end(args);
