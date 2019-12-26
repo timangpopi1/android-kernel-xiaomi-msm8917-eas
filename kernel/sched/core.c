3b5b6e11cf423c095166ab809f9f02aaf7174a0c
Mhmmdfadlyas <mhmmdfas@pm.me>
2019-12-27 06:01:25 +0700

    Revert "rcu: Stop disabling interrupts in scheduler fastpaths"
    This reverts commit 3c4cf1d045f3ac5e024d33eb828315c64b911f1b.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3180,6 +3180,7 @@ static void __sched __schedule(void)
 	preempt_disable();
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
+	rcu_note_context_switch();
 	prev = rq->curr;
 
 	/*
@@ -3197,9 +3198,6 @@ static void __sched __schedule(void)
 
 	if (sched_feat(HRTICK))
 		hrtick_clear(rq);
-
-	local_irq_disable();
-	rcu_note_context_switch();
 
 	/*
 	 * Make sure that signal_pending_state()->signal_pending() below
